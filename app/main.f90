b3e06b2294ceda1b55345d6bd4e01fae35097295
Carlos Une <brocolis@eml.cc>
2021-12-12 02:20:18 -0300

    Remove TOML mode

--- app/main.f90
+++ app/main.f90
@@ -42,7 +42,7 @@ integer :: n
 integer :: ier
 
 call usage()
-call set_args(' --toml:T F --registry "null" --force-download:F F', help_text, version_text)
+call set_args(' --registry "null" --force-download:F F', help_text, version_text)
 arg_count = size(arg)
 
 if (arg_count .eq. 0) then
@@ -100,26 +100,14 @@ if (config_ok) then
     end do
 end if
 
-if (lget('toml')) then
-    select case(arg_count)
-    case(1)
-       call table_add(tbl, trim(arg(1)))
-    case(2)
-       call table_add(tbl, trim(arg(1)), trim(arg(2)))
-    case default
-       write(*,*)'wrong number of arguments for "--toml" mode'
-       write(*,'(a)')help_text
-    end select
+if (lget('verbose')) then
+    do loop=1, arg_count
+        call table_info(tbl, trim(arg(loop)))
+    end do
 else
-    if (lget('verbose')) then
-        do loop=1, arg_count
-            call table_info(tbl, trim(arg(loop)))
-        end do
-    else
-        do loop=1, arg_count
-            call table_search(tbl, trim(arg(loop)))
-        end do
-    end if
+    do loop=1, arg_count
+        call table_search(tbl, trim(arg(loop)))
+    end do
 end if
 
 contains
@@ -289,32 +277,22 @@ help_text=[character(len=80) :: &
 ! '12345678901234567890123456789012345678901234567890123456789012345678901234567890', &
 & 'NAME                                                                            ', &
 & '   fpm-search(1) - display available FPM packages                               ', &
+& '                                                                                ', &
 & 'SYNOPSIS                                                                        ', &
 & '   syntax:                                                                      ', &
+& '   fpm-search SEARCH_STRING [--verbose] [--registry URI] [--force-download]     ', &
 & '                                                                                ', &
-& '    fpm-search SEARCH_STRING(s) [--verbose] [--registry URI] [--force-download] ', &
-& '     or                                                                         ', &
-& '    fpm-search --toml PACKAGE_NAME [TAG]                                        ', &
 & 'DESCRIPTION                                                                     ', &
 & '   Search for and display information describing fpm (Fortran Package Manager)  ', &
 & '   packages registered in the fpm repository at                                 ', &
+& '   https://github.com/fortran-lang/fpm-registry                                 ', &
 & '                                                                                ', &
-& '      https://github.com/fortran-lang/fpm-registry                              ', &
 & 'OPTIONS                                                                         ', &
-& ' SEARCH MODE:                                                                   ', &
 & '    SEARCH_STRING  A string used to match package descriptions.                 ', &
 & '                   It is case-sensitive. The default is "", causing all         ', &
 & '                   registered packages to be displayed.                         ', &
 & '    --verbose,-V   give more-detailed information about the packages matching   ', &
 & '                   SEARCH_STRING.                                               ', &
-& '                                                                                ', &
-& ' TOML ENTRY MODE:                                                               ', &
-& '    --toml,-T      instead of an fpm project description generate the line      ', &
-& '                   needed to be added to the "fpm.toml" file in order to use    ', &
-& '                   the specified external package in your fpm project.          ', &
-& '    PACKAGE_NAME   specific case-sensitive fpm package name.                    ', &
-& '    TAG            A git(1) tag name can optionally follow the PACKAGE_NAME     ', &
-& '                   when using the --toml switch.                                ', &
 & '                                                                                ', &
 & ' DOCUMENTATION:                                                                 ', &
 & '    --help,-h      display this help and exit                                   ', &
@@ -325,10 +303,6 @@ help_text=[character(len=80) :: &
 & '                                                                                ', &
 & '  fpm-search hash                                                               ', &
 & '  fpm-search pixel --verbose                                                    ', &
-& '                                                                                ', &
-& '  fpm-search M_color --toml                                                     ', &
-& '  fpm-search --toml datetime v1.7.0                                             ', &
-& '                                                                                ', &
 & '  fpm-search     # list all package descriptions                                ', &
 & '  fpm-search -V  # describe all packages in detail                              ', &
 & '']
@@ -462,38 +436,6 @@ subroutine table_info(tbl, pattern)
             200 format(a16, ': ', a, ' = { git = "', a, '", tag="', a, '" }')
             300 format(a16, ': ', a, ' = { git = "', a, '" }')
         end if
-    end do
-
-end subroutine
-
-subroutine table_add(tbl, name, tag)
-    type(fhash_tbl_t), intent(in) :: tbl
-    character(len=*), intent(in) :: name
-    character(len=*), intent(in), optional :: tag
-    type(package_t) :: pkg
-    logical :: r
-    integer :: i
-    integer :: num_buckets, num_items
-
-    call tbl%stats(num_buckets, num_items)
-
-    do i = 1, num_items
-        call table_get_package(tbl, fhash_key(i), pkg, r)
-
-        if (name .eq. pkg%name) then
-            if (present(tag)) then
-                if (tag .eq. pkg%git_tag) then
-                    print 100, pkg%name, pkg%git, pkg%git_tag
-                    return
-                end if
-            else
-                print 200, pkg%name, pkg%git
-                return
-            end if
-        end if
-
-        100 format(a, ' = { git = "', a, '", tag="', a, '" }')
-        200 format(a, ' = { git = "', a, '" }')
     end do
 
 end subroutine
